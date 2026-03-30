ff32e4375b78822add249c8b0667f0ec26c0bdfd
AbhijitIndware <abhijit.singha@indware.com>
2026-03-30 16:37:49 +0530

    feat: enhance TeamDetailScreen with navigation props and SafeAreaView layout

--- src/screens/SO/Sales/TeamDetailScreen.tsx
+++ src/screens/SO/Sales/TeamDetailScreen.tsx
@@ -1,11 +1,37 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import PageHeader from '../../../components/ui/PageHeader';
+import {SafeAreaView} from 'react-native';
+import {flexCol} from '../../../utils/styles';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {SoAppStackParamList} from '../../../types/Navigation';
+import {Colors} from '../../../utils/colors';
 
-const TeamDetailScreen = () => {
+type NavigationProp = NativeStackNavigationProp<
+  SoAppStackParamList,
+  'TeamDetailScreen'
+>;
+
+type Props = {
+  navigation: NavigationProp;
+  route: any;
+};
+const TeamDetailScreen = ({navigation, route}: Props) => {
   return (
-    <View>
+    <SafeAreaView
+      style={[
+        flexCol,
+        {
+          flex: 1,
+          backgroundColor: Colors.lightBg,
+        },
+      ]}>
+      <PageHeader
+        title={'Team Detail'}
+        navigation={() => navigation.goBack()}
+      />
       <Text>TeamDetailScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
